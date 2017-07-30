c08d969567c0c679df16996bb07bc5c74584fbaa
SayedSadat <Shafiq_Sadat@hotmail.com>
2017-07-30 23:10:59 +0430

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 					level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-updater = Updater(token='TOKEN') # put here the bot's token
+updater = Updater(token='254861371:AAGsE9KA6xkOGqB2YBSsMf83bPxDnsLfnMQ') # put here the bot's token
 dispatcher = updater.dispatcher
 
 ydl_opts = {
